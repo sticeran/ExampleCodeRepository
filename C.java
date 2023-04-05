3bf17f0aafdbca09a1ab968fcdd4b14427a72b3b
sticeran <5702676@qq.com>
2023-04-05 16:15:11 +0800

    Revert "Modify comments and code formats"
    This reverts commit a748a386baee61387a6db052fc06e330eeb20eaf.

--- C.java
+++ C.java
@@ -1,14 +1,8 @@
 package main;
 
 public class C {
-
-	/**
-	 * 函数功能： 参数合法性检查
-	 */
+	
 	public void FUNC1(int para) {
-		// 如果参数大于0，则打印合法
-		if (para > 0) {
-			System.out.print("legal");
-		}
+		if(para > 0){System.out.print("legal");}
 	}
 }
