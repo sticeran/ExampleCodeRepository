a748a386baee61387a6db052fc06e330eeb20eaf
sticeran <5702676@qq.com>
2023-04-05 15:56:16 +0800

    Modify comments and code formats

--- C.java
+++ C.java
@@ -1,8 +1,14 @@
 package main;
 
 public class C {
-	
+
+	/**
+	 * 函数功能： 参数合法性检查
+	 */
 	public void FUNC1(int para) {
-		if(para > 0){System.out.print("legal");}
+		// 如果参数大于0，则打印合法
+		if (para > 0) {
+			System.out.print("legal");
+		}
 	}
 }
