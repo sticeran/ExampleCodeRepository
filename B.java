dd7e00bff2944ea6a04a16cd933aa54c1eb93f58
sticeran <5702676@qq.com>
2023-04-05 16:02:16 +0800

    Delete sub function of B.java

--- B.java
+++ B.java
@@ -5,7 +5,5 @@ public class B {
 	public static void sum(int a, int b) {
 		System.out.println(a + b);
 	}
-
-	//该bug经查与sub函数无关
-	public static void sub(int a, int b) {System.out.println(a - b);}
+	
 }
