095e5f9215324bad3630f9b921a6567670c2cbcc
sticeran <5702676@qq.com>
2023-04-05 15:41:58 +0800

    BugFix and add annotation

--- dev1/A.java
+++ dev1/A.java
@@ -2,15 +2,21 @@ package main;
 
 public class A {
 
-	private int sign = -1;
-	
+	private int sign;
+
 	public void FUNC1(int para, int para1, int para2) {
-		if (sign < 0) {
+		sign = para;// 修复bug
+		if (sign > 0) {
 			B.sub(para1, para2);
 		}
 	}
-	
+
+	/**
+	 * 函数功能： 该函数计算两个整型参数相减并输出
+	 */
 	public void FUNC2(int para1, int para2) {
-		if (para1 > para2){System.out.println(para1-para2);}
+		if (para1 > para2) {
+			System.out.println(para1 - para2);
+		}
 	}
 }
